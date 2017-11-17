213787c5347f2a9cda9a09657755e46b57a3e86e
v_lixgong <v_lixgong@tencent.com>
2017-11-17 18:01:16 +0800

    实现向下功能

--- websocket-tetris/demo3/script/game.js
+++ websocket-tetris/demo3/script/game.js
@@ -27,13 +27,71 @@ var Game = function () {
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0]
     ]
-    //当前方块
+    // 当前方块
     var cur;
-    //下一个方块
+    // 下一个方块
     var next;
     var gameDivs = [];
     var nextDivs = [];
-    //初始化Div
+    // 检测点是否合法
+    var check = function (pos, x, y) {
+        if(pos.x + x < 0) {
+            return false;
+        } else if(pos.x + x >= gameData.length) {
+            return false;
+        } else if(pos.y + y < 0) {
+            return false;
+        } else if(pos.y + y >= gameData[0].length) {
+            return false;
+        } else if (gameData[pos.x + x][pos.y + y] == 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    // 检测数据是否合法
+    var isValid = function (pos, data) {
+        for(var i = 0; i < data.length; i ++) {
+            for(var j = 0; j < data[0].length; j ++) {
+                if(data[i][j] != 0) {
+                    if(!check(pos, i, j)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    // 清除数据
+    var clearData = function () {
+        for(var i = 0; i < cur.data.length; i++) {
+            for(var j = 0; j < cur.data[0].length; j++) {
+                if(check(cur.origin, i, j)) {
+                    gameData[cur.origin.x + i][cur.origin.y + j] = 0;
+                }
+            }
+        }
+    }
+    // 设置数据
+    var setData = function () {
+        for(var i = 0; i < cur.data.length; i++) {
+            for(var j = 0; j < cur.data[0].length; j++) {
+                if(check(cur.origin, i, j)) {
+                    gameData[cur.origin.x + i][cur.origin.y + j] = cur.data[i][j];
+                }
+            }
+        }
+    }
+    // 下移
+    var down = function () {
+        if(cur.canDown(isValid)) {
+            clearData();
+            cur.origin.x = cur.origin.x + 1;
+            setData();
+            refreshDiv(gameData, gameDivs);
+        }
+    }
+    // 初始化Div
     var initDiv = function (container, data, divs) {
         for(var i = 0; i < data.length; i++) {
             var div = [];
@@ -70,9 +128,13 @@ var Game = function () {
         next = new Square();
         initDiv(gameDiv, gameData, gameDivs);
         initDiv(nextDiv, next.data, nextDivs);
+        cur.origin.x = 10;
+        cur.origin.y = 5;
+        setData();
         refreshDiv(gameData, gameDivs);
         refreshDiv(next.data, nextDivs);
     }
     //导出API
     this.init = init;
+    this.down = down;
 }
--- websocket-tetris/demo3/script/local.js
+++ websocket-tetris/demo3/script/local.js
@@ -2,6 +2,21 @@ var Local = function () {
     // 游戏对象
     var game;
     // 开始
+    var bindKeyEvent = function () {
+        document.onkeydown = function (e) {
+            if(e.keyCode == 38) {   // up
+
+            } else if (e.keyCode == 39) { // right
+                
+            } else if (e.keyCode == 40) { // down
+                game.down();
+            } else if (e.keyCode == 37) { // left
+                
+            } else if (e.keyCode == 32) { // space
+                
+            }
+        }
+    }
     var start = function () {
         var doms = {
             gameDiv: document.getElementById('game'),
@@ -9,6 +24,7 @@ var Local = function () {
         }
         game = new Game();
         game.init(doms);
+        bindKeyEvent();
     }
     // 导出API
     this.start = start;
